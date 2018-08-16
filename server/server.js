a1c6aed1cc057ead568600c09b7615586f3171d9
Rachit Anand <rachitanand@Rachits-MacBook-Pro.local>
2018-08-15 22:59:56 -0400

    Added /todos/:id logic

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -27,8 +28,29 @@ app.get('/todos', (req, res) => {
             res.send({todos})
         }, (e) => {
             res.status(400).send(e);
-        })
-})
+        });
+});
+
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    if ( !ObjectID.isValid(id)) {
+        res.status(404).send('Invalid ID');
+    } else {
+        Todo   
+            .findById(id)
+            .then((todo) => {
+                if(todo){
+                    res.status(200).send({todo})
+                } else {
+                    res.status(404).send();
+                }
+
+                
+            }, (e) => {
+                res.status(400).send();
+            });
+    }
+});
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
